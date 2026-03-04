af71c6a9851de63776f0c209174160cbad307904
UriMelian <Urimelian28@gmail.com>
2026-03-03 22:11:36 -0300

    Add files via upload

---
+++ API de gestion de datos/config.py
@@ -0,0 +1,8 @@
+DB_HOST = (
+"aws-0-us-west-2.pooler.supabase.com"  #  Aca va el host de tu base de datos
+)
+DB_PORT = "6543"  #  Este es siempre el mismo
+DB_NAME = "APIgd"  #  Aca va el nombre de tu base de datos
+DB_USER = "urimelian28@gmail.com"  #  Aca va el usuario de tu base de datos
+DB_PASSWORD = "RoMa201221"  #  Aca va la contraseña de tu base de datos
+DB_SSLMODE = "require"  #  Este siempre es lo mismo
